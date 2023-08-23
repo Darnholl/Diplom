623e4c97e8c2ad152e8bc2ee0c89d3e3c2618bdc
Alexey Blinov <Rasiel1989@gmail.com>
2023-08-23 18:33:35 +0300

    ready for deploy

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 const chalk = require("chalk");
 const cors = require("cors");
+const path = require("path");
 const routes = require("./routes");
 
 const app = express();
@@ -15,11 +16,15 @@ app.use("/api", routes);
 
 const PORT = config.get("port") ?? 8080;
 
-// if(process.env.NODE_ENV === 'production'){
-//   console.log('production')
-// }else{
-//   console.log('development')
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client")));
+
+  const indexPath = path.join(__dirname, "client", "index.html");
+
+  app.get("*", (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
 
 async function start() {
   try {
